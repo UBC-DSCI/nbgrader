9a9dd62de5cc71da241a731515d0fda19bebd90f
Trevor Campbell <trevor.d.campbell@gmail.com>
2019-09-12 16:28:26 -0700

    added install autotest script; to be removed later once pkg is stable

---
+++ install_autotest.sh
@@ -0,0 +1,4 @@
+cp nbgrader/converters/generate_assignment.py ~/.local/lib/python3.6/site-packages/nbgrader/converters
+cp nbgrader/preprocessors/instantiatetests.py ~/.local/lib/python3.6/site-packages/nbgrader/preprocessors
+cp nbgrader/preprocessors/__init__.py ~/.local/lib/python3.6/site-packages/nbgrader/preprocessors
+
