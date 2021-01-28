a52e47409903542e31c4e022c22286ec03082721
Trevor Campbell <trevor.d.campbell@gmail.com>
2021-01-28 12:07:36 -0800

    added clearsolutions patch to enable R again...

--- install_autotest.sh
+++ install_autotest.sh
@@ -1,4 +1,6 @@
 cp nbgrader/converters/generate_assignment.py ~/.local/lib/python3.8/site-packages/nbgrader/converters
 cp nbgrader/preprocessors/instantiatetests.py ~/.local/lib/python3.8/site-packages/nbgrader/preprocessors
 cp nbgrader/preprocessors/__init__.py ~/.local/lib/python3.8/site-packages/nbgrader/preprocessors
+#for some reason, the new nbgrader removed R from clearsolutions... 
+cp nbgrader/preprocessors/clearsolutions.py ~/.local/lib/python3.8/site-packages/nbgrader/preprocessors
 
